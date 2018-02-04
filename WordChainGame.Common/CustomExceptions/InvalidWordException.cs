316072360f30c22375705f1b8546ab373d117e63
migrenaa <mimonova13@gmail.com>
2018-02-05 00:36:22 +0200

    added unit tests for topicService

--- WordChainGame.Common/CustomExceptions/InvalidWordException.cs
+++ WordChainGame.Common/CustomExceptions/InvalidWordException.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace WordChainGame.Common.CustomExceptions
 {
+    using System;
+
     public class InvalidWordException : Exception
     {
         public InvalidWordException()
